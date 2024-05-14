34ab6fa2a98d59cfe0ffcf9d42cfc69c7d4c8b14
Jesus Silveira <silveirajesus69@gmail.com>
2024-05-14 02:19:04 -0400

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let tempPeople;
+    let tempAge = 0;
+    let olderAge = 0;
+    let olderPerson = people[0].name;
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
 
+    for(let i = 0; i < people.length; i++){
+
+        tempPeople = people[i];
+
+        if(!tempPeople.yearOfDeath){
+            people[i].yearOfDeath = currentYear;
+        }
+
+        tempAge = tempPeople.yearOfDeath - tempPeople.yearOfBirth;
+
+        if(tempAge > olderAge){
+            olderAge = tempAge;
+            olderPerson = tempPeople.name;
+        }
+    }
+    return olderPerson;
 };
 
 // Do not edit below this line
