2e623e4bdc4a2270271e0139387f8f11ddfe12ba
Jesus Silveira <silveirajesus69@gmail.com>
2024-05-14 01:30:19 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = 0;
+    const majorNum = (num1 > num2)? num1 : num2;
+    const minorNum = (num1 < num2)? num1 : num2;
 
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    else if(typeof num1 !== "number" || typeof num2 !== "number" ){
+        return "ERROR";
+    }
+    else{
+        for(let i = minorNum; i <= majorNum; i++){
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
