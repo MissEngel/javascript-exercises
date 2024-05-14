8ccddf85d9de9eeb88b20f94b6c8cd37cb9f3b98
Jesus Silveira <silveirajesus69@gmail.com>
2024-05-14 01:28:27 -0400

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(+num === 0){
+        return 0;
+    }
+    else if(+num < 0){
+        return "OOPS"
+    }
 
+    let total = 1;
+    let firstLastNum = 1;
+    let secondLastdNum = 1;
+
+    for(let i = 3; i <= +num ; i++){
+        total = firstLastNum + secondLastdNum;
+        firstLastNum = secondLastdNum;
+        secondLastdNum = total;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
