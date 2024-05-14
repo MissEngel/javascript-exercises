5efd6ab9c1e58ae6a3980dc636a61a34d569f871
Jesus Silveira <silveirajesus69@gmail.com>
2024-05-14 01:27:45 -0400

    complete all other exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().trim();
+    str = str.replace(/[^a-zA-Z0-9]/g, "")
+    newStr = "";
 
+    for(let i = str.length - 1; i >= 0; i--){
+        newStr += str[i];
+    }
+    return (str === newStr);
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    result = [];
+    let tempBook = "";
 
+    for(let i = 0; i < books.length; i++){
+        tempBook = books[i];
+        result.push(tempBook['title'])
+    }
+
+    return result
 };
 
 // Do not edit below this line
